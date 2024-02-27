c3f0b2d348f0e50f7644dbef0b33c6607cf65e32
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-02-28 02:47:29 +0530

    Adds text animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,29 @@
 import bgImg from "./../assets/background.png"
 import foreImg from "./../assets/foreground.png"
+import { useEffect, useRef, useState } from "react"
 
 export default function Landing(){
+  const [offset, setOffset] = useState(20);
+  const textRef = useRef(null)
+
+  useEffect(()=>{
+    let totalHeight = document.body.offsetHeight;
+
+    const scrollHandler = (e) => {
+      let x = offset - (window.pageYOffset / totalHeight * 100);
+      console.log(x)
+      textRef.current.style.left = `${x}%`
+    }
+    
+    window.addEventListener("scroll", scrollHandler)
+    return () => window.removeEventListener("scroll", scrollHandler)
+  }, [])
+
+
   return(
     <div className=" h-screen w-screen relative">
       <img src={bgImg} className="w-screen h-screen absolute" />
-      <div className=" absolute -translate-y-1/2 top-1/2  w-full  text-center flex flex-col items-center">
+      <div className={`absolute -translate-y-1/2 top-1/2  w-full  text-center flex flex-col items-center left-[${offset}%] landing-text`} ref={textRef}>
         <h1 className="text-[7rem] "> EMBRACING CANE </h1>
         <h3 className="text-[2.5rem] w-1/2">A SUSTAINABLE STEP TOWARDS A DURBALE FUTURE</h3>
       </div>
@@ -13,3 +31,4 @@ export default function Landing(){
     </div>
   )
 }
+
